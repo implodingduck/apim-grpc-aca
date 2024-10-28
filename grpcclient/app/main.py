e32cd00583371b87903ede0633462b1c18386c3c
implodingduck <zsek320@gmail.com>
2024-10-28 14:13:46 -0500

    messing around with traces

--- grpcclient/app/main.py
+++ grpcclient/app/main.py
@@ -10,6 +10,12 @@ from app.pb.diceroller_pb2_grpc import DicerollerStub
 
 from azure.monitor.opentelemetry import configure_azure_monitor
 
+from opentelemetry import trace, baggage
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+from opentelemetry.baggage.propagation import W3CBaggagePropagator
+
 from logging import getLogger
 from logging.config import dictConfig
 
@@ -43,6 +49,9 @@ dictConfig(log_config)
 logger = getLogger("api-logger")
 logger.info(os.environ.get("APPLICATIONINSIGHTS_CONNECTION_STRING"))
 
+# trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+tracer = trace.get_tracer(__name__) 
+
 app = FastAPI()
 
 origins = [
@@ -57,27 +66,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.middleware("http")
+async def ensure_w3c_header(request: Request, call_next):
+    logger.info(f"Request received: {request.url}")
+    logger.info(f"Middleware Headers: {request.headers}")
+    response = await call_next(request)
+    return response
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/grpc")
-async def get_grpc(dice: str = "1d6"):
-    #async with grpc.aio.insecure_channel(os.environ.get("GRPC_ENDPOINT")) as channel:
-    if os.environ.get("GRPC_ENDPOINT").find(":443") > -1:
-        credentials = grpc.ssl_channel_credentials()
-        async with grpc.aio.secure_channel(os.environ.get("GRPC_ENDPOINT"), credentials) as channel:
-            stub = DicerollerStub(channel)
-            logger.info(f"Calling gRPC endpoint {os.environ.get('GRPC_ENDPOINT')}")
-            response = await stub.Roll(Dice(dice=dice))
-            logger.info(f"Response from gRPC endpoint {response.input} | {response.result}")
-            return { "input": response.input, "result": response.result }   
-    else:
-        async with grpc.aio.insecure_channel(os.environ.get("GRPC_ENDPOINT")) as channel:
-            stub = DicerollerStub(channel)
-            logger.info(f"Calling gRPC endpoint {os.environ.get('GRPC_ENDPOINT')}")
-            response = await stub.Roll(Dice(dice=dice))
-            logger.info(f"Response from gRPC endpoint {response.input} | {response.result}")
-            return { "input": response.input, "result": response.result }   
+async def get_grpc(request: Request, dice: str = "1d6"):
 
+    logger.info(f"Pre Headers: {request.headers}")
+    with tracer.start_as_current_span("grpcdiceroll"):
+        ctx = baggage.set_baggage("hello", "world")
+
+        headers = {}
+        W3CBaggagePropagator().inject(headers, ctx)
+        TraceContextTextMapPropagator().inject(headers, ctx)
+        logger.info(f"Post Headers: {headers}")
+        if os.environ.get("GRPC_ENDPOINT").find(":443") > -1:
+            credentials = grpc.ssl_channel_credentials()
+            async with grpc.aio.secure_channel(os.environ.get("GRPC_ENDPOINT"), credentials) as channel:
+                stub = DicerollerStub(channel)
+                logger.info(f"Calling gRPC endpoint {os.environ.get('GRPC_ENDPOINT')}")
+                response = await stub.Roll(Dice(dice=dice), metadata=[('grpc-trace-bin', headers['traceparent'].encode())])
+                logger.info(f"Response from gRPC endpoint {response.input} | {response.result}")
+                return { "input": response.input, "result": response.result }   
+        else:
+            async with grpc.aio.insecure_channel(os.environ.get("GRPC_ENDPOINT")) as channel:
+                stub = DicerollerStub(channel)
+                logger.info(f"Calling gRPC endpoint {os.environ.get('GRPC_ENDPOINT')}")
+                response = await stub.Roll(Dice(dice=dice), metadata=[('grpc-trace-bin', headers['traceparent'].encode())])
+                logger.info(f"Response from gRPC endpoint {response.input} | {response.result}")
+                return { "input": response.input, "result": response.result }   
+
+FastAPIInstrumentor.instrument_app(app, tracer_provider=trace.get_tracer_provider())
